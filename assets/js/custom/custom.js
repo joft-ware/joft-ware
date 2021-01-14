94f6cfbc7b511c006918a2095e5d467c0279483b
joft-ware <zxc91911003@gmail.com>
2021-01-15 00:06:14 +0900

    - POST added. - Comments added - Profile widget added

---
+++ assets/js/custom/custom.js
@@ -0,0 +1,13 @@
+
+    (function(d, h, m){
+    var js, fjs = d.getElementsByTagName(h)[0];
+    if (d.getElementById(m)){return;}
+    js = d.createElement(h); js.id = m;
+    js.onload = function(){
+    window.makerWidgetComInit({
+    position: "left",
+    widget: "fzb5mbogl8v3h1dw-oljvddicsnw7big4-l4xe7jjlofrejhs1"
+})};
+    js.src = "https://makerwidget.com/js/embed.js";
+    fjs.parentNode.insertBefore(js, fjs)
+}(document, "script", "dhm"))
